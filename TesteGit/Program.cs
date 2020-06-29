9dac2ae478381505525ca1722b503b4ada55383d
Americo Alves <m_eco_21@hotmail.com>
2020-06-29 12:11:49 -0300

    Projeto teste para Git

---
+++ TesteGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, esse é meu teste Git");
+            Console.WriteLine("Digite um numero inteiro");
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i=0;i<n;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
